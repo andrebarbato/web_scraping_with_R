6ef42dd8f4bd45b476755e49f7d2e62398b7edb0
andrebarbato <andrebarbato@yahoo.com.br>
2022-10-28 23:23:32 -0300

    update

--- 01-web-scraping.R
+++ 01-web-scraping.R
@@ -4,7 +4,7 @@
 
 library(rvest)
 library(yfR)
-library(dplyr)
+library(tidyverse)
 
 # get IBOVESPA tickers
 
@@ -26,4 +26,20 @@ df_ibov_fund <- df_ibov_fund[[1]]
 
 glimpse(df_ibov_fund)
 
+names(df_ibov_fund) <- c("ticker", "Cotacao", "P_L", "P_VP", "PSR", "Div.Yield",
+                         "P_Ativo", "P_Cap.Giro", "P_EBIT", "P_Ativ Circ.Liq", "EV_EBIT", 
+                         "EV_EBITDA" , "Mrg Ebit", "Mrg. Liq.", "Liq. Corr.",  "ROIC", "ROE",
+                         "Liq.2meses", "Patrim. Liq", "Div.Brut_Patrim.","Cresc. Rec.5a")
 
+n <- dim(df_ibov_fund)[2]
+
+df_ibov_fund_original <- df_ibov_fund
+df_ibov_fund_original -> df_ibov_fund
+
+
+df_ibov_fund$Cotacao <- sapply(X = df_ibov_fund$Cotacao, 
+                               FUN = str_replace_all, pattern = '.',
+                               replacement = '')
+
+df_ibov_fund <- as.numeric(sub(x = df_ibov_fund$Cotacao,pattern = ',', 
+                            replacement = '.'))
